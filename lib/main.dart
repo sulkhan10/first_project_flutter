b8e05a28993331c5210e83352245b70788d1e41b
sulkhan10 <sulkhangalangsakti@gmail.com>
2023-10-04 00:31:21 +0700

    minor change

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class _TaskListState extends State<TaskList> {
             }
           },
           decoration: InputDecoration(
-            hintText: 'Enter a to-do task here',
+            hintText: 'Enter a to-do task',
           ),
         ),
         Expanded(
